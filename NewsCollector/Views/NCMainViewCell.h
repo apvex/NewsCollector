6a8432a10f120d41adc3aafa18ea909ef99adafa
Pavel Anikin <apvex@me.com>
2016-10-20 21:35:10 +0300

    [Add:] New Class NCMainViewCell of custom cell on main view

---
+++ NewsCollector/Views/NCMainViewCell.h
@@ -0,0 +1,13 @@
+//
+//  NCMainViewCell.h
+//  NewsCollector
+//
+//  Created by Pavel Anikin on 20.10.16.
+//  Copyright © 2016 APVEX, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NCMainViewCell : UITableViewCell
+
+@end
---
+++ NewsCollector/Views/NCMainViewCell.m
@@ -0,0 +1,24 @@
+//
+//  NCMainViewCell.m
+//  NewsCollector
+//
+//  Created by Pavel Anikin on 20.10.16.
+//  Copyright © 2016 APVEX, LLC. All rights reserved.
+//
+
+#import "NCMainViewCell.h"
+
+@implementation NCMainViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
